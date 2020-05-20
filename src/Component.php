6f4ef72ae2bbd0d768f5c1afebfb6bc0d6c68fb2
Leonardo Losoviz <leo@getpop.org>
2020-05-20 18:06:58 +0800

    Initialize Component manually

---
+++ src/Component.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\CommentMeta;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    // const VERSION = '0.1.0';
+
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoP\Meta\Component::class,
+            \PoP\Comments\Component::class,
+        ];
+    }
+
+    public static function getDependedMigrationPlugins(): array
+    {
+        return [
+            'migrate-commentmeta',
+        ];
+    }
+}
